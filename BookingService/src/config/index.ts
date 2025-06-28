fef5c99bb709cfd562e56c4fb90a338a569487ea
anil8109 <anil810966@gmail.com>
2025-06-28 23:07:45 +0530

    added optimized code of booking service

--- BookingService/src/config/index.ts
+++ BookingService/src/config/index.ts
@@ -2,16 +2,19 @@
 import dotenv from 'dotenv';
 
 type ServerConfig = {
-    PORT: number
+    PORT: number,
+    REDIS_SERVER_URL: string,
+    LOCK_TTL: number
 }
 
 function loadEnv() {
-    dotenv.config();
-    console.log(`Environment variables loaded`);
+    dotenv.config()
 }
 
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+    PORT: Number(process.env.PORT) || 3001,
+    REDIS_SERVER_URL: process.env.REDIS_SERVER_URL || 'redis://localhost:6379',
+    LOCK_TTL: Number(process.env.LOCK_TTL) || 50000
 };
